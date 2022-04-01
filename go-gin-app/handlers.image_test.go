e52a79ef3aea64673329950c97b2f281ddd0a271
OKAWARI <724735116@qq.com>
2022-03-31 21:29:15 -0400

    Backend: Pass handlers.image_test.go.

--- go-gin-app/handlers.image_test.go
+++ go-gin-app/handlers.image_test.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetAvatar(t *testing.T) {
+	w := httptest.NewRecorder()
+	r := getRouter(true)
+	http.SetCookie(w, &http.Cookie{Name: "token", Value: "%7B%22username%22%3A%22user1%22%2C%22password%22%3A%22pass1%22%7D"})
+	r.GET("/avatar/:username", ensureLoggedIn(), getAvatar)
+	req, _ := http.NewRequest("GET", "/avatar/user1", nil)
+	req.Header = http.Header{"Cookie": w.Result().Header["Set-Cookie"]}
+	r.ServeHTTP(w, req)
+	if w.Code != http.StatusOK {
+		t.Fail()
+	}
+}
